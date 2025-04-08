5acb1d5b8fb6e4fb79baccb7decf77f9cae3b37c
212301 <212301@1220-010-22>
2025-04-08 16:50:36 +0300

    Добавьте файлы проекта.

---
+++ AuthorizationWPF/Pages/AuthorizePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthorizationWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizePage.xaml
+    /// </summary>
+    public partial class AuthorizePage : Page
+    {
+        public AuthorizePage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginTb.Text;
+            var password = PasswordTb.Password;
+            var loginUser = "1";
+            var passwordUser = "1";
+            if (login == loginUser && password == passwordUser)
+            {
+                MessageBox.Show("Здравствуй админушка");
+                NavigationService.Navigate(new AnketaPage());
+            }
+            else MessageBox.Show("От тебя воняет");
+        }
+    }
+}
